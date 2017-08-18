8b1bb07235597fdaf1d4662f3c04ea02a1213486
HsTime <1497707753@qq.com>
2017-08-18 09:54:24 +0800

    添加时间

--- Text.cpp
+++ Text.cpp
@@ -1,13 +1,17 @@
 #include"huffman.h"
 #include"Filecampress.h"
+#include<time.h>
 int main()
 {
+	clock_t start,finish;
+	double cptime;
 	/*string filename = "E:\\Campress\\DSC_0004.JPG";*/
 	string filename = "E:\\Campress\\1.txt";
 	/*string filename = "E:\\Campress\\campress.txt";*/
 	//string filename = "E:\\Campress\\2017田径运动会秩序册20170410.doc";
 	/*string filename = "E:\\Campress\\MC阿哲 - 别无所求.mp3";*/
 	FileCompress fc;
+	start = clock();
 	fc.CompressHuffCode(filename.c_str());
 
 	/*string unfilename = "E:\\Campress\\DSC_0004.huff";*/
@@ -17,5 +21,8 @@ int main()
 	//string unfilename = "E:\\Campress\\MC阿哲 - 别无所求.huff";
 	FileCompress unfc;
 	unfc.UnCompressHuffCode(unfilename.c_str());
+	finish = clock();
+	cptime = (double)(finish - start) / CLOCKS_PER_SEC;
+	cout << "压缩解压缩时间为：" << cptime << "秒" << endl;
 	return 0;
 }
